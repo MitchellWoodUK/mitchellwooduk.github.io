790e0895686d3de4a6c8cd6f5c78fac3f7350a36
MitchellWoodUK <80631581+MitchellWoodUK@users.noreply.github.com>
2022-04-28 10:39:50 +0100

    Add project files.

---
+++ Assignment2Project/Models/CustomUserModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment2Project.Models
+{
+    public class CustomUserModel : IdentityUser
+    {
+        [Required]
+        public string Fname { get; set; }
+        [Required]
+        public string Sname { get; set; }
+    }
+}
---
+++ Assignment2Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assignment2Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
